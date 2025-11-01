1cea473571e0f8d487a85ba787d16f489a4c21d7
Bryan Lai <bryanlais@gmail.com>
2025-11-01 15:49:00 +0800

    fix(nix): apply versioning patch

--- overlays/mods.nix
+++ overlays/mods.nix
@@ -19,12 +19,15 @@ with prev;
   nixPackage = (nixVersions.latest.appendPatches [
     (fetchpatch2 {
       name = "fix-zsh-completions.patch";
-      url = "https://github.com/bryango/nix/commit/956fffdd6f196fc5b61057e19d43a8b275369649.patch";
-      hash = "sha256-u8OqATgFJO5zrDQx/v7hRRyk+W5QBWOH5QF1fV6/xTg=";
+      url = "https://github.com/NixOS/nix/commit/f434a3e3c637fcfd371e62f6167ac9bc136e012e.patch?full_index=1";
+      hash = "sha256-bBH9e4Eg6viEM7PlKGK4OrKFVzXt/80w/koZbAZmslE=";
     })
-  ]).overrideAllMesonComponents (_finalScope: _prevScope@ { version, ... }: {
-    version = lib.head (lib.splitString "+" version);
-  });
+    (fetchpatch2 {
+      name = "fix-darwin-ld-versioning.patch";
+      url = "https://github.com/NixOS/nix/commit/a24df3d4e5ba03570294abfb49660b5258337484.patch?full_index=1";
+      hash = "sha256-LBlDXe7WMlCQFZF/SotBp0vLK24VNtoTS2l+Xct0dP0=";
+    })
+  ]);
   # nixPackage = lixPackageSets.latest.lix;
   # nixPackage = nixVersions.stable.overrideAttrs ({ patches ? [  ], ... }: {
   #   patches = patches ++ [
